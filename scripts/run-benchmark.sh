fdb37b42e362db248a167511c628654ded47ccde
ctsai12 <ctsai12@illinois.edu>
2017-03-29 12:58:51 -0500

    merged

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -7,17 +7,10 @@
 # Running example:
 # scripts/run-benchmark.sh es config/xlwikifier-tac.config
 #
-# Two options for LANG: "es" and "zh"
+# Three options for LANG: "en", "es", and "zh"
 #
 # Using configuration file config/xlwikifier-tac.config with all TAC2016 test documents, you will get the following performance:
 #
-# Spanish (es):
-# Mention Span: Precision:0.8793 Recall:0.8007 F1:0.8382
-# Mention Span + Entity Type: Precision:0.8538 Recall:0.7775 F1:0.8139
-#
-# Chinese (zh):
-# Mention Span: Precision:0.8683 Recall:0.7236 F1:0.7894
-# Mention Span + Entity Type: Precision:0.8350 Recall:0.6959 F1:0.7591
 
 LANG=$1
 CONFIG=$2
